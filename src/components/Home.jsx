8163b0047dc1b9d84778dbfd08440ec4734749d4
Tanvir1407 <mt14050@gmail.com>
2025-02-10 23:19:23 +0600

    initialize React app with Vite, Tailwind CSS, and Ant Design components

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center h-screen text-6xl'>
+      <Button type="primary" shape="circle">
+        A
+      </Button>
+    </div>
+  )
+}
