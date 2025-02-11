c28eecbc240d2ec51e197dfbb9b0b59349db3499
Abdullah Al Mehmud <timehmud121@gmail.com>
2025-02-11 22:45:34 +0600

    added user profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/LandingPage/Home";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home></Home>} />
+        <Route path="/userProfile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
